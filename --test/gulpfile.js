19b2203c45bb5fb1ca7549b36ad1e9bfd4dd70f1
Binatik <47430210+Binatik@users.noreply.github.com>
2020-08-03 02:13:12 +0300

    Глобальные изменения. Полный рабочий код.

--- --test/gulpfile.js
+++ --test/gulpfile.js
@@ -1,29 +1,73 @@
 const formats = '{img,svg,webp,jpg,png}';
 //Получаем пути для чтения  объектов.
 const getPaths = (root) => ({
-    html: `${root}/*.html`,
+    html: [`${root}/*.html`, `!${root}/+*.html`],
     image: `${root}/image/**/*.${formats}`,
     fonts: `${root}/fonts/*.ttf`,
-    styles: `${root}/styles/`,
-    scripts: `${root}/scripts/`,
+    styles: `${root}/styles/*.css`,
+    scripts: [`${root}/scripts/*.js`, `!${root}/scripts/+*.js`],
 
 });
 
-console.log(getPaths('development').html);
-
-//Слушаем события типа stream.
-function pathListWhichListen(root) {
-    const lists = [
-        `${root}/**/*.html`,
-        `${root}/image/**/*.${formats}`,
-        '${root}/**/*.css',
-        '${root}/**/*.js',
-    ]
-    return lists;
-}
 //Удаления проекта из папки ways
-function pathDelete(...ways) {
+function pathDelete(ways) {
     return `./${ways}/`
+}
+
+//Выполняем task 
+function getTask(task) {
+    if (task === 'html') {
+        return src(getPaths('development').html)
+            .pipe(fileinclude()) 
+            .pipe(webpHTML())
+    }
+
+    if (task === 'image') {
+        return src(getPaths('development').image)  
+        .pipe(dest(`build/image/`)) 
+        .pipe(webp({ 
+            quality: 75,
+        }))
+        .pipe(imagemin({ 
+            progressive: true,  
+            interlaced: true,
+            optimizationLevel: 3,
+        }))
+    }
+
+    if (task === 'fonts') {
+        return src(getPaths('development').fonts)
+    }
+
+    if (task === 'styles') {
+        return src(getPaths('development').styles)
+            .pipe(fileinclude())
+            .pipe(autoprefixer({
+                overrideBrowserslist: ['last 5 versions'],
+                cascade: true
+            }))
+            .pipe(dest(`build/styles/`))
+            .pipe(gcmq())
+            .pipe(cleanCSS({
+                compatibility: 'ie8'
+            }))
+            .pipe(rename({
+                extname: '.min.css'
+            }))
+    }
+    if (task === 'scripts') {
+        return src(getPaths('development').scripts)
+            .pipe(fileinclude()) 
+            .pipe(dest(`build/scripts/`)) 
+            .pipe(babel({
+                presets: ['@babel/env']
+              }))
+            .pipe(uglify()) 
+            .pipe(rename({
+                extname: '.min.js'
+            }))
+
+    }
 }
 
 //Подключаем  gulp.
@@ -33,40 +77,73 @@ const {
 } = require('gulp'),
 
     gulp = require('gulp'),
-    browserSync = require('browser-sync').create(), 
-    fileinclude = require('gulp-file-include');
+    browserSync = require('browser-sync').create(),
+    fileinclude = require('gulp-file-include'),
+    autoprefixer = require('gulp-autoprefixer'),
+    gcmq = require('gulp-group-css-media-queries'),
+    cleanCSS = require('gulp-clean-css'),
+    rename = require("gulp-rename"),
+    uglify = require('gulp-uglify'), 
+    babel = require('gulp-babel'), 
+    imagemin = require('gulp-imagemin'), 
+    webp = require('gulp-webp'), 
+    webpHTML = require('gulp-webp-html'),
+    del = require('del');
 
-const updatePage = () => browserSync.init({
-    server: {
-        baseDir: './build/'
-    },
-}); 
+function updatePage() { 
+    browserSync.init({
+        server: {
+            baseDir: './build/'
+        },
+    })  
 
-// 
-const build = (name, dirname = '') => ( 
-    src(getPaths('development')[name])  
-    .pipe(fileinclude())
-    .pipe(dest(`build/${dirname}`)) 
-    .pipe(src(getPaths(`build/${dirname ? `${dirname}/*.*` : ''}`)[name])) 
-    .pipe(browserSync.stream()) 
-    );
+    gulp.watch(`development/**/*.html`, getHtml);
+    gulp.watch(`development/image/**/*.${formats}`, getImage);
+    gulp.watch(`development/**/*.css`, getStyles);
+    gulp.watch(`development/**/*.js`, getScripts); 
+}
 
 
-const getHtml = () => (
-    build('html')
-); 
 
-const getImage = () => (
-    build('image', 'image')
-); 
 
-const getFonts = () => (
-    build('fonts', 'fonts')
-); 
+
+//Методы
+const delite = () => del(pathDelete('build'));
+
+function build(func = '', name, dirname = '', ) {
+    return func()
+        .pipe(dest(`build/${dirname}`))
+        .pipe(src(getPaths(`build/${dirname ? `${dirname}/*.*` : ''}`)[name]))
+        .pipe(browserSync.stream())
+}
+
+
+function getHtml() {
+    return build(() => getTask('html'), 'html');
+}
+
+function getImage() {
+    return build(() => getTask('image'), 'image', 'image');
+}
+
+function getFonts() {
+    return build(() => getTask('fonts'), 'fonts', 'fonts');
+}
+
+function getStyles() {
+    return build(() => getTask('styles'), 'styles', 'styles');
+}
+
+function getScripts() {
+    return build(() => getTask('scripts'), 'scripts', 'scripts');
+}
+
+
+
+
 
 //Комбинации.  
-gulp.watch(pathListWhichListen('development'), getHtml, getImage, getFonts);  
-const defaultGulp = gulp.series(getHtml, getImage, getFonts, updatePage);
+const defaultGulp = gulp.series(delite, getHtml, getImage, getFonts, getStyles, getScripts, updatePage);
 
 //module exports.arbitraryName = function/variable 
-exports.default = defaultGulp; 
+exports.default = defaultGulp;
